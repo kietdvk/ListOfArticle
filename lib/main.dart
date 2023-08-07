c8e2e49fd4ba9a75431ee25d75490dd77f62430a
Mr Key <kietdvkvn@gmail.com>
2023-08-07 18:22:45 +0700

    List of Article final

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'List Of Article',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: Homepage(),
     );
   }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_listof/screens/home/timeline_card.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({
@@ -9,6 +10,8 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        shadowColor: const Color.fromARGB(0, 255, 255, 255),
         leadingWidth: 180,
         toolbarHeight: 80,
         leading: Padding(
@@ -92,67 +95,6 @@ class Homepage extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class TimelineCard extends StatelessWidget {
-  TimelineCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Text(
-              "08:00 AM",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Expanded(
-            flex: 8,
-            child: Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [Colors.white, Colors.blue]),
-                borderRadius: BorderRadius.circular(3),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CardArticle extends StatelessWidget {
-  const CardArticle({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsetsDirectional.only(end: 5),
-      width: 90,
-      height: 90,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        gradient: LinearGradient(
-          begin: Alignment.centerRight,
-          end: Alignment.centerLeft,
-          colors: [Colors.white, Colors.blue],
-        ),
       ),
     );
   }
